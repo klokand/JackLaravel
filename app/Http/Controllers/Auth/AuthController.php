3c9146600a0dc095484bdfff8445c49302f55813
klokand <xjz520223@gmail.com>
2015-06-26 15:37:42 +1000

    remove the registe funciton from guest

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -35,5 +35,10 @@ class AuthController extends Controller {
 		$this->middleware('guest', ['except' => 'getLogout']);
 	}
 
-	
+	public function getRegister(){
+		return redirect('/');
+	}
+	public function postRegister(){
+		return redirect('/');
+	}
 }
