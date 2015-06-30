d43dc91a8b0c35734a15f75b75160a97031bf69d
klokand <xjz520223@gmail.com>
2015-06-30 17:01:48 +1000

    route testing again

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,10 @@
 |
 */
 
-/*
+
 Route::get('/', 'WelcomeController@index');
-*/
-Route::get('/','MasterController@indexPage');
+
+Route::get('index','MasterController@indexPage');
 Route::get('home', 'HomeController@index');
 Route::controllers([
 	'auth' => 'Auth\AuthController',
@@ -29,6 +29,6 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::post('updateServices','ContentController@updateServices');
     Route::post('updateContact','ContentController@updateContact');
 });
-Route::any('{path?}','MasterController@indexPage');
+//Route::any('{path?}','MasterController@indexPage');
 
 
